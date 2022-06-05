7d2e7dca93f2b992e4006aa06aaad2cfc0a05716
sywoon <sywoon@163.com>
2022-06-05 18:46:45 +0800

    add babel es6 -> es5

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
+// Rollup plugins
+import babel from 'rollup-plugin-babel';
 
 export default {
-	input: './src/main.js',
-	output: {
-		file: './dist/js/main.min.js',
-		format: 'cjs'
-	}
+  input: './src/main.js',
+  output: {
+    file: './dist/js/main.min.js',
+    format: 'cjs'   //iife
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'  // 排除node_module下的所有文件
+    })
+  ]
 }
